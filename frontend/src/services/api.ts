07249ea670693846dc9b52f3d7fa77568ddeb3e0
Allen Pinto <pinto.allen05@gmail.com>
2025-10-31 00:32:50 +0530

    Updated vite_url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import type { CreateBookingData } from '../types/index';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://highway-delite-kehn.onrender.com/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
